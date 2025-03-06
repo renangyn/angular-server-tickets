36ab779cdaa24e41f267fdd0d32bc3ad409ce0a6
webgaha <webgaha@gmail.com>
2025-03-06 09:44:22 -0300

    first commit

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,23 @@
+import { Component, ElementRef, inject, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
+})
+export class ControlComponent {
+  @Input({ required: true }) label!: string;
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('clicked');
+ 
+  }
+}
