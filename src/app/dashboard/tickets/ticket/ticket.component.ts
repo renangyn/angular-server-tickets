2589256f843260239cf6166fcf59b6d7903972dc
webgaha <webgaha@gmail.com>
2025-03-12 09:18:44 -0300

    subindo

--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, input, Input, signal } from '@angular/core';
+import { Ticket } from './ticket.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
-
+  //data = input.required<Ticket>;
+  @Input ()data!: Ticket;
+  
+  detailsVisible = signal(false)
+  onTogleDetails(){
+    this.detailsVisible.update((wasVisible) => !wasVisible)
+}
 }
